85a1890bd98e25b2a581afdbd6f6770d245beae1
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-01-13 13:27:30 +0530

    ingress controller-provider.tf,main.tf,variables.tf,parameters.tf

--- 06-acm/parameters.tf
+++ 06-acm/parameters.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "acm_certificate_arn" {
+resource "aws_ssm_parameter" "acm_certificate_arn" { #uploading acm_certificate_arn to aws_ssm_parameter
   name  = "/${var.project_name}/${var.environment}/acm_certificate_arn"
   type  = "String"
   value = aws_acm_certificate.expense.arn
