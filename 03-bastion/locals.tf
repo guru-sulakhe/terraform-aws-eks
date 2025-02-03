39695affc6a1806bfec8c25e6613e9231b78df0c
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-03 14:45:04 +0530

    adding user data in bastion instance

--- 03-bastion/locals.tf
+++ 03-bastion/locals.tf
@@ -1,3 +1,6 @@
 locals {
   public_subnet_id = element(split(",", data.aws_ssm_parameter.public_subnet_ids.value), 0)
 }
+
+#split() function is used for producing a list by separating a single string using a given delimiter.
+#element() function is used to retrieves a single element from a list.
