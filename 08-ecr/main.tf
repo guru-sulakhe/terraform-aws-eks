76e8c1bc2245767d108a9c314552e3c8c4c9944b
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-12-01 12:11:14 +0530

    adding comments

--- 08-ecr/main.tf
+++ 08-ecr/main.tf
@@ -1,7 +1,7 @@
 #ecr  It can be used to store and manage Docker images by creating individual repositories in AWS
 resource "aws_ecr_repository" "backend" { #backend ecr repository
   name                 = "${var.project_name}-backend"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "IMMUTABLE" #IMMUTABLE will update the docker image if the tags are updated to the dockerfile
 
   image_scanning_configuration {
     scan_on_push = true
@@ -10,7 +10,7 @@ resource "aws_ecr_repository" "backend" { #backend ecr repository
 
 resource "aws_ecr_repository" "frontend" { #frontend ecr repository
   name                 = "${var.project_name}-frontend"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "IMMUTABLE" #IMMUTABLE will update the docker image if the tags are updated to the dockerfile
 
   image_scanning_configuration {
     scan_on_push = true
